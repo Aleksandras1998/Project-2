acb509af08d940ffb545a4b4fefbbc04c65bf2f7
Aleksandras1998 <aleksandras.saraskinas@gmail.com>
2023-04-17 22:52:33 +0200

    Mostly done.
    Have some problems with indexing of Nx, Ny, Nz in the array_from_file

---
+++ Own binary file/Data plot function.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def dataPlot(data, statistic):
+    
+    if statistic == 'Mean':
+        
+    if statistic == 'Variance':
+        
+    if statistic == 'Cross correlation':
+    
+        
